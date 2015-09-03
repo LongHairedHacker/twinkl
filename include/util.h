39aa340f82f5202408b0601b5b56980bbb3c73ce
LongHairedHacker <sebastian@sebastians-site.de>
2015-09-03 12:47:54 +0200

    Stripped return values, for portability (os_memset on esp8266 does not have them)

--- include/util.h
+++ include/util.h
@@ -3,7 +3,7 @@
 
 #include <stdlib.h>
 
-void* __attribute__((weak)) twinkl_memset(void *ptr, int value, size_t num);
-void* __attribute__((weak)) twinkl_memcpy(void * destination, const void * source, size_t num);
+void __attribute__((weak)) twinkl_memset(void *ptr, int value, size_t num);
+void __attribute__((weak)) twinkl_memcpy(void *destination, const void *source, size_t num);
 
 #endif
--- util.c
+++ util.c
@@ -2,10 +2,10 @@
 
 #include "util.h"
 
-void* twinkl_memset(void *ptr, int value, size_t num) {
-	return memset(ptr, value, num);
+void twinkl_memset(void *ptr, int value, size_t num) {
+	memset(ptr, value, num);
 }
 
-void* twinkl_memcpy(void *destination, const void *source, size_t num) {
-	return memcpy(destination, source, num);
+void twinkl_memcpy(void *destination, const void *source, size_t num) {
+	memcpy(destination, source, num);
 }
